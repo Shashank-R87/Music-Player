13e35660b3672df9ba18a670d1eecba4a0cfb95b
Shashank <shashankr8724@gmail.com>
2023-12-10 13:34:01 +0530

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import loadingData from "../lottie/loading.json"
-import Lottie from 'react-lottie'
+import Lottie from 'lottie-react'
 
 function Loading() {
 
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: loadingData,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
     return (
         <div>
-            <Lottie options={defaultOptions} width={250} height={250}/>
+            <Lottie animationData={loadingData} loop={true} width={250} height={250}/>
         </div>
     )
 }
